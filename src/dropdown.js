c193b962b29ac3039462934320535d6c8587b994
OktawianMac <144891794+OktawianMac@users.noreply.github.com>
2023-11-21 07:54:25 +0100

    Add files via upload

---
+++ src/dropdown.js
@@ -0,0 +1,31 @@
+function toggleDropdownOptions(element) {
+    var options = element.querySelector('.dropdown-options');
+    options.classList.toggle("active");
+}
+
+var dropdownMenus = document.querySelectorAll('.dropdown-menu');
+
+dropdownMenus.forEach(function (menu) {
+    var span = menu.querySelector('span');
+    var options = menu.querySelector('.dropdown-options');
+
+    span.addEventListener('mouseover', function () {
+        options.classList.add("active");
+    });
+
+    span.addEventListener('mouseout', function (event) {
+        if (!menu.contains(event.relatedTarget) && !options.contains(event.relatedTarget)) {
+            options.classList.remove("active");
+        }
+    });
+
+    options.addEventListener('mouseover', function () {
+        options.classList.add("active");
+    });
+
+    options.addEventListener('mouseout', function (event) {
+        if (!menu.contains(event.relatedTarget) && !span.contains(event.relatedTarget)) {
+            options.classList.remove("active");
+        }
+    });
+});
