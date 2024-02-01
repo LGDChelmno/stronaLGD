5fcb31ecab677cb3e12aa9ffbc338cf4b1f1eddd
OktawianMac <maciejewskioktawian@gmail.com>
2024-02-01 14:54:52 +0100

    Update cookies.js

--- src/cookies.js
+++ src/cookies.js
@@ -7,7 +7,7 @@ const analyticsId = 'G-9FZN7890RP';
 
 // Dodaj dynamicznie skrypt do strony z identyfikatorem Google Analytics
 const scriptElement = document.createElement('script');
-scriptElement.src = `https://www.googletagmanager.com/gtag/js?id=${analyticsId}`;
+scriptElement.src = `https://www.googletagmanager.com/gtag/js?id=G-9FZN7890RP`;
 scriptElement.async = true;
 
 // Obsługa zdarzenia załadowania skryptu
