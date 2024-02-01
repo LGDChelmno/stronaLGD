688441800350b8002aa2b60011ad022446dd1c9c
OktawianMac <maciejewskioktawian@gmail.com>
2024-02-01 14:28:48 +0100

    Update cookies.js

--- src/cookies.js
+++ src/cookies.js
@@ -24,6 +24,24 @@ function checkCookiesAccepted() {
     return localStorage.getItem('cookiesAccepted') === 'true';
 }
 
+// Funkcja do akceptowania plików cookies
+function acceptCookies() {
+    localStorage.setItem('cookiesAccepted', 'true');
+    // Sprawdź stan po zaakceptowaniu plików cookies
+    checkCookiesAccepted();
+    // Uruchom Google Analytics po zaakceptowaniu plików cookies
+    if (!isAnalyticsDisabled()) {
+        gtag('js', new Date());
+        gtag('config', analyticsId);
+    }
+}
+
+// Funkcja do wyłączania Google Analytics
+function disableAnalytics() {
+    gtag('config', analyticsId, { 'send_page_view': false });
+    localStorage.setItem('analyticsDisabled', 'true');
+}
+
 // Sprawdzanie, czy Google Analytics zostało wyłączone przez użytkownika
 function isAnalyticsDisabled() {
     return localStorage.getItem('analyticsDisabled') === 'true';
